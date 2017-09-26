8f47d0c72c8b4830ac439099251aa78a92b2a319
Faldy Ikhwan Fadila <faldy.if@gmail.com>
2017-09-26 14:06:39 +0700

    Adding validation

--- routes/web.php
+++ routes/web.php
@@ -11,43 +11,97 @@
 |
 */
 
+use Illuminate\Support\Facades\Validator;
+
 Route::get('/list', function () {
     $users = \App\User::all();
     return json_encode($users);
 });
 Route::get('/add', function (\Illuminate\Http\Request $request) {
-    $nama = $request->nama;
-    $email = $request->email;
-    $user = \App\User::create([
-        'nama' => $nama,
-        'email' => $email,
+    $validator = Validator::make($request->all(), [
+        'nama' => 'required',
+        'email' => 'required|email',
     ]);
 
     $result = array();
-    $result['status'] = true;
-    $result['result'] = $user;
-    return json_encode($result);
-});
-Route::get('/edit', function (\Illuminate\Http\Request $request) {
-    $id = $request->id;
-    $nama = $request->nama;
-    $email = $request->email;
-    $user = \App\User::find($id)
-        ->update([
+
+    if($validator->fails()) {
+        $result['status'] = false;
+        $result['error'] = 'Bad Parameter';
+        return json_encode($result);
+    } else {
+        $nama = $request->nama;
+        $email = $request->email;
+        $user = \App\User::create([
             'nama' => $nama,
             'email' => $email,
         ]);
 
-    $result = array();
-    $result['status'] = $user;
-    $result['result'] = \App\User::find($id);
-    return json_encode($result);
+        $result['status'] = true;
+        $result['result'] = $user;
+        return json_encode($result);
+    }
 });
-Route::get('/delete', function (\Illuminate\Http\Request $request) {
-    $id = $request->id;
-    $user = \App\User::find($id)->delete();
+Route::get('/edit', function (\Illuminate\Http\Request $request) {
+    $validator = Validator::make($request->all(), [
+        'nama' => 'required',
+        'email' => 'required|email',
+        'id' => 'required|integer',
+    ]);
 
     $result = array();
-    $result['status'] = $user;
-    return json_encode($result);
+
+    if($validator->fails()) {
+        $result['status'] = false;
+        $result['error'] = 'Bad Parameter';
+        return json_encode($result);
+    } else {
+        $id = $request->id;
+        $nama = $request->nama;
+        $email = $request->email;
+        $user = \App\User::find($id);
+
+        if($user != null) {
+            $action = $user->update([
+                'nama' => $nama,
+                'email' => $email,
+            ]);
+
+            $result['status'] = $action;
+            $result['result'] = \App\User::find($id);
+            return json_encode($result);
+        } else {
+            $result['status'] = false;
+            $result['error'] = 'User not Found!';
+            return json_encode($result);
+        }
+    }
+});
+Route::get('/delete', function (\Illuminate\Http\Request $request) {
+    $validator = Validator::make($request->all(), [
+        'id' => 'required|integer',
+    ]);
+
+    $result = array();
+
+    if($validator->fails()) {
+        $result['status'] = false;
+        $result['error'] = 'Bad Parameter';
+        return json_encode($result);
+    } else {
+        $id = $request->id;
+        $user = \App\User::find($id);
+
+        if($user != null) {
+            $action = $user->delete();
+
+            $result['status'] = $action;
+            $result['result'] = "Delete successful!";
+            return json_encode($result);
+        } else {
+            $result['status'] = false;
+            $result['error'] = 'User not found!';
+            return json_encode($result);
+        }
+    }
 });
